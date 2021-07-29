68ac8096df8157fce65d5982b2db5061956cbdc4
Yunus Emre Görmüş <ygormus173@gmail.com>
2021-07-29 11:28:41 +0300

    first codes

---
+++ exercise1/main.py
@@ -0,0 +1,10 @@
+var1= 'emre'
+var2='emre'
+
+
+num1=4
+num2=4
+
+print(var1,var2)
+cal= num1*num2
+print(num1,num2,cal)
---
+++ main.py
@@ -0,0 +1,28 @@
+age = 30
+
+print (age)
+age=40
+print(age)
+
+friend_age=23
+
+countries_visited=90
+PI=3.14581
+Radian = 180 / PI
+
+print (countries_visited)
+print (Radian)
+
+float_division = 12 /3
+integer_division = 13 //5
+print(float_division)
+print(integer_division)
+
+remainder = 13 % 5
+print (remainder)
+
+x=37
+remainder = x % 2
+print (remainder)
+
+print ('hello world')
